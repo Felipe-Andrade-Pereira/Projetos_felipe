a97f8526500a6c4cff58e51b70394f135a43d3bd
Felipe Andrade Pereira <felipeandradepereira99@gmail.com>
2023-07-09 23:18:17 -0300

    commit inicial

---
+++ Tabuada.py
@@ -0,0 +1,14 @@
+a = int(input('digite um número para saber sua tabuada'))
+print('----------------------')
+print('{} X {}'.format(1,1*a))
+print('{} X {}'.format(2,2*a))
+print('{} X {}'.format(3,3*a))
+print('{} X {}'.format(4,4*a))
+print('{} X {}'.format(5,5*a))
+print('{} X {}'.format(6,6*a))
+print('{} X {}'.format(7,7*a))
+print('{} X {}'.format(8,8*a))
+print('{} X {}'.format(9,9*a))
+print('{} X {}'.format(10,10*a))
+
+
